a28085deeb98393b538837a732f6515b58ff11fc
f-teruhisa <teru_fukumoto@outlook.jp>
2021-10-03 15:41:44 +0900

    feat: Implement a loop to puts what need from the results of API response

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ requests: HTTP Clients for API request
 datetime: Get today's date
 """
 import datetime
+import json
 import requests
 
 URL = "https://disclosure.edinet-fsa.go.jp/api/v1/documents.json"
@@ -12,6 +13,13 @@ params = {
   "type": 2
 }
 
-res = requests.get(URL, params=params, verify=False)
+req = requests.get(URL, params=params, verify=True)
 
-print(res.text)
+response_json = json.loads(req.text)
+results = response_json["results"]
+
+for result in results:
+    # Guard empty documents data in response body
+    if result['edinetCode'] is None:
+        continue
+    print(result['docID'], result['docDescription'],result['filerName'])
